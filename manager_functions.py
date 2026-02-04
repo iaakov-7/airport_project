0a705bb65d29ac279c7821271a364672226cee5e
moshe gertner <mg8540113@gmail.com>
2026-02-04 12:24:13 +0200

    Add Admin_authentication function

--- manager_functions.py
+++ manager_functions.py
@@ -1,1 +1,9 @@
-pass
+import csv
+
+def Admin_authentication(name_file):
+    with open(name_file, 'r',encoding = "utf-8") as file:
+        reader = csv.reader(file)
+        all_data = list(reader)
+    return all_data            
+
+
