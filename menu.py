c1e7a4671e571d362139e38879adcf9d42861978
moshe gertner <mg8540113@gmail.com>
2026-02-04 14:59:27 +0200

    Indentation repair

--- menu.py
+++ menu.py
@@ -5,18 +5,19 @@ def run_menu():
     print("1. Admin")
     print("2. Passenger")
 
-list_of_users = Return_file_credentials_in_list("credentials.csv")
-choice = input("Enter your choice (1 or 2): ")
+    list_of_users = Return_file_credentials_in_list("credentials.csv")
+    choice = input("Enter your choice (1 or 2): ")
 
-if choice == "1":
-    print("Redirecting to Admin Portal")
-    user_name = input("Enter your user name")
-    password = input("Enter a password")
-    is_admin_verification(user_name , password , list_of_users) 
-    
-elif choice == "2":
-    print("Where would you like to fly this time?")
-    #מחכה לפונקצית נוסע
-else:
-    print("please choose 1 or 2 ")
+    if choice == "1":
+        print("Redirecting to Admin Portal")
+        user_name = input("Enter your user name")
+        password = input("Enter a password")
+        is_admin_verification(user_name , password , list_of_users) 
+        
+    elif choice == "2":
+        print("Where would you like to fly this time?")
+        #מחכה לפונקצית נוסע
+    else:
+        print("please choose 1 or 2 ")
    
+run_menu()
