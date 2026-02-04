7ae99f5a208c42a4bc57571adc8217f5ffc5af82
moshe gertner <mg8540113@gmail.com>
2026-02-04 15:11:44 +0200

    refactor: improve menu display and input prompts for better clarity.

--- menu.py
+++ menu.py
@@ -1,17 +1,19 @@
 from  manager_functions import Return_file_credentials_in_list , is_admin_verification
 def run_menu():
+    print(" ")
     print("--- Welcome to the Flight Management System ---")
-    print("Please choose your login type:")
-    print("1. Admin")
-    print("2. Passenger")
+    print("        Please choose your login type:")
+    print("                  1. Admin")
+    print("                  2. Passenger")
+    print(" ")
 
     list_of_users = Return_file_credentials_in_list("credentials.csv")
     choice = input("Enter your choice (1 or 2): ")
 
     if choice == "1":
         print("Redirecting to Admin Portal")
-        user_name = input("Enter your user name")
-        password = input("Enter a password")
+        user_name = input("Enter your user name: ")
+        password = input("Enter a password:")
         is_admin_verification(user_name , password , list_of_users) 
         
     elif choice == "2":
