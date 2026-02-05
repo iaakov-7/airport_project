5f4ce24e7ae4f36ace4207b43693866214c7ee2b
moshe gertner <mg8540113@gmail.com>
2026-02-05 14:47:57 +0200

    feat: Add `balance_update` function to `passenger_functions.py` to modify `budget.txt` and apply an initial budget deduction.

--- passenger_functions.py
+++ passenger_functions.py
@@ -1,1 +1,10 @@
-pass
+import manager_functions
+
+def balance_update(final_price):
+    Current_balance = int(manager_functions.num_budget())
+    Current_balance -= final_price
+
+    with open("budget.txt", "w") as f:
+        f.write(str(Current_balance))
+
+
