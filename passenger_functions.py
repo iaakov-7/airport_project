1e2e44266de53a297f5d9028232dfc6827bd3ebd
moshe gertner <mg8540113@gmail.com>
2026-02-05 15:09:23 +0200

    feat: add `Origin_and_destination_verification` function to handle and validate origin/destination country input.

--- passenger_functions.py
+++ passenger_functions.py
@@ -8,3 +8,21 @@ def balance_update(final_price):
         f.write(str(Current_balance))
 
 
+def Origin_and_destination_verification():
+    while True:
+        destination_country = input("Enter destination country: ")
+        origin_country = input("Enter origin country: ")
+        if destination_country == origin_country:
+            print("Destination country cannot be the same as origin country")
+        elif destination_country != origin_country:
+            List_of_available_destinations = Show_Available()
+            for line in List_of_available_destinations:
+                if line[0] == destination_country and line[1] == origin_country:
+                    selected_destinations = [line[0], line[1]]
+                    return selected_destinations
+                else:
+                    print("Destination country or origin country are not available")
+                    return None
+
+
+    
