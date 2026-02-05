d108e913a1789ed3ad9c6b93d9c7fd03d695f9b6
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-05 14:24:53 +0200

    added file continents.csv

--- files_management.py
+++ files_management.py
@@ -1,7 +1,7 @@
 import os
 
-def check_file(file_name1 ,file_name2 , file_name3 , file_name4 ):
-   if os.path.exists(file_name1) and os.path.exists(file_name2) and os.path.exists(file_name3) and os.path.exists(file_name4):
+def check_file(file_name1 ,file_name2 , file_name3 , file_name4 , file_name5 ):
+   if os.path.exists(file_name1) and os.path.exists(file_name2) and os.path.exists(file_name3) and os.path.exists(file_name4) and os.path.exists(file_name5):
         return True
    else:
         return False
