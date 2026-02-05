d95bae15bed42cb12e7829ac8217cd4fe0024001
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-05 15:13:51 +0200

    Show_Available to user

--- passenger_functions.py
+++ passenger_functions.py
@@ -1,1 +1,20 @@
-pass
+import json
+
+def Show_Available():
+    with open("available_lines.json", "r") as file:
+        data = json.load(file)
+        routes_list = []
+        print("Available Flight Routes:")
+        
+        
+        for i, line in enumerate(data["available_lines"], start=1):
+            origin = line["origin_airport"]
+            destination = line["destination_airport"]
+            
+            
+            routes_list.append([origin, destination])
+            
+       
+            print(f"[{i}] {origin} -> {destination}")
+            
+        return routes_list
